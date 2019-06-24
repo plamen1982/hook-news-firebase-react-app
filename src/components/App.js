52192d7653bbf64fa0d9f2196c941041851ca01d
plamen1982 <plamen.hristov@ymail.com>
2019-06-24 22:00:22 +0300

    Header functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,26 @@ import CreateLink from './Link/CreateLink';
 import SearchLinks from './Link/SearchLinks';
 import LinkDetail from './Link/LinkDetail';
 import LinkList from './Link/LinkList';
+import Header from './Header';
 
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path='/' render={() => <Redirect to="/new/1" />}></Route>
-          <Route path='/create' component={CreateLink}></Route>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/forgot' component={ForgotPassword}></Route>
-          <Route path='/search' component={SearchLinks}></Route>
-          <Route path='/top' component={LinkList}></Route>
-          <Route path='/new/:page' component={LinkList}></Route>
-          <Route path='/link/:linkId' component={LinkDetail}></Route>
-        </Switch>
+        <div className="app-container">
+            <Header />
+            <div className="route-container">
+              <Switch>
+                <Route exact path='/' render={() => <Redirect to="/new/1" />}></Route>
+                <Route path='/create' component={CreateLink}></Route>
+                <Route path='/login' component={Login}></Route>
+                <Route path='/forgot' component={ForgotPassword}></Route>
+                <Route path='/search' component={SearchLinks}></Route>
+                <Route path='/top' component={LinkList}></Route>
+                <Route path='/new/:page' component={LinkList}></Route>
+                <Route path='/link/:linkId' component={LinkDetail}></Route>
+              </Switch>
+            </div>
+        </div>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { withRouter, NavLink } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+    return (
+        <div className="header">
+            <div className="flex">
+                <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+                <NavLink to="/" className="header-title">
+                    Hook News
+                </NavLink>
+                <NavLink to="/" className="header-link">
+                    new
+                </NavLink>
+                <div className="divider">|</div>
+                <NavLink to="/top" className="header-link">
+                    top
+                </NavLink>
+                <div className="divider">|</div>
+                <NavLink to="/search" className="header-link">
+                    search
+                </NavLink>
+                <div className="divider">|</div>
+                <NavLink to="/create" className="header-link">
+                    submit
+                </NavLink>
+            </div>
+            <div className="flex">
+              <NavLink to="/login" className="header-link">
+                    login
+              </NavLink>
+            </div>
+        </div>
+    );
 }
 
-export default Header;
+export default withRouter(Header);
