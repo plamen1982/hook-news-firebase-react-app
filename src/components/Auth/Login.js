49b7a2bb8ac060d27918fcb0cd95b13b30d153b5
plamen1982 <plamen.hristov@ymail.com>
2019-06-24 23:40:05 +0300

    custom hook created for form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import useFormValidation from './useFormValidation';
+
+
+//Will provide the INITIAL_STATE to our custom hook(useFormValidation), so we can have reusability to the custom hook
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
+  const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE);
   const [login, setLogin] = useState(true);
   return (
     <div>
       <h2 className="mv3">{login ? "Login": "Create Account"}</h2>
-      <form className="flex flex-column">
-        {!login && <input type="text" placeholder="Your name" autoComplete="off" />}
-        <input type="email" placeholder="Your email" autoComplete="off" />
-        <input type="password" placeholder="Your password" autoComplete="off" />
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && <input 
+          onChange={handleChange}
+          value={values.name}
+          name="name"
+          type="text" placeholder="Your name" autoComplete="off" />}
+        <input 
+          onChange={handleChange}
+          value={values.email}
+          name="email"
+          type="email" placeholder="Your email" autoComplete="off" />
+        <input 
+          onChange={handleChange}   
+          value={values.password}
+          name="password"     
+          type="password" placeholder="Your password" autoComplete="off" />
         <div className="flex mt3">
           <button type="" className="button pointer mr2">
             Submit
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+    const[values, setValues] = useState(initialState);
+
+    function handleChange(event) {
+        event.persist();
+        const { target: { name, value } } = event;
+        setValues(previousValues => ({
+            ...previousValues,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log({ values })
+    }
+    return { handleSubmit, handleChange, values };
+}
 
 export default useFormValidation;
