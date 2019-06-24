41c7969e91430de3236004df348d77b02179b273
plamen1982 <plamen.hristov@ymail.com>
2019-06-24 20:51:57 +0300

    Setting up the router and route path with coresponding components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './Auth/Login';
+import ForgotPassword from './Auth/ForgotPassword';
+import CreateLink from './Link/CreateLink';
+import SearchLinks from './Link/SearchLinks';
+import LinkDetail from './Link/LinkDetail';
+import LinkList from './Link/LinkList';
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to="/new/1" />}></Route>
+          <Route path='/create' component={CreateLink}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/forgot' component={ForgotPassword}></Route>
+          <Route path='/search' component={SearchLinks}></Route>
+          <Route path='/top' component={LinkList}></Route>
+          <Route path='/new/:page' component={LinkList}></Route>
+          <Route path='/link/:linkId' component={LinkDetail}></Route>
+        </Switch>
+    </Router>
   );
 }
 
