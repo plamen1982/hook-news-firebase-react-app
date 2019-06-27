a6026dd20cbb96afe17f9dca87328d15ecc57a08
plamen1982 <plamen.hristov@ymail.com>
2019-06-27 23:00:28 +0300

    Tracking user session with useAuth hook

--- src/components/Auth/useAuth.js
+++ src/components/Auth/useAuth.js
@@ -1,5 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import firebase from '../../firebase';
 
-function useAuth() {}
+function useAuth() {
+    const [ authUser, setAuthUser ] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = firebase.auth.onAuthStateChanged(user => {
+            if(user) {
+                setAuthUser(user);
+            } else {
+                setAuthUser(null);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+    
+    return authUser;
+}
 
 export default useAuth;
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useAuth from '../Auth/useAuth';
 
 function LinkList(props) {
+  const user = useAuth();
+  console.log({ user });
   return <div>LinkList</div>;
 }
 
