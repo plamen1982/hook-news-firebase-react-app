be3fddc3c7539e5618da5a79857ad8359c9d519d
plamen1982 <plamen.hristov@ymail.com>
2019-07-09 19:46:27 +0300

    List Links functionality created

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { getDomain } from '../../utils';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 
-function LinkItem() {
-  return <div>LinkItem</div>;
+function LinkItem({ link, index, showCount }) {
+  return (
+    <div className="flex items-start mt2">
+      <div className="flex items-center">
+        {showCount && <span className="gray">{index}.</span>}
+        <div className="vote-button">▲</div>
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} <span className="link">({getDomain(link.url)})</span>
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes by {link.postedBy.name} about {distanceInWordsToNow(link.created)}
+          {" | "}
+          <Link to={`/link/${link.id}`}></Link>
+          {link.comments.length > 0
+            ? `${link.comments.length} comments`
+            : "discuss"
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LinkItem;
