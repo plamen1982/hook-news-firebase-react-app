c0c310341146df3133456ccce96618b3a3def6c5
plamen1982 <plamen.hristov@ymail.com>
2019-07-13 08:18:47 +0300

    Ordering, Sorting Links in Firebase

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -4,13 +4,17 @@ import LinkItem from './LinkItem';
 
 function LinkList(props) {
   const { firebase: { db } } = useContext(FirebaseContext);
-  const [links, setLinks] = useState([])
+  const [links, setLinks] = useState([]);
+  const isNewPage = props.location.pathname.includes('new');
+
   useEffect(() => {
     getLinks();
   }, []);
 
   function getLinks() {
-    db.collection('links').onSnapshot(handleSnapshotLinks);
+    db.collection('links')
+    .orderBy('created', 'desc')
+    .onSnapshot(handleSnapshotLinks);
   }
 
   function handleSnapshotLinks(snapshotLinks) {
@@ -20,9 +24,24 @@ function LinkList(props) {
     setLinks(links);
   }
 
+  function renderLinks() {
+    if(isNewPage) {
+      return links;
+    }
+   const topLinks = [...links].sort((link1, link2) => link2.votes.length - link1.votes.length);
+   return topLinks;
+  }
+
   return (
     <div>
-      {links.map((link, index) => <LinkItem key={link.id} showCount={true} link={link} index={index + 1} />)}
+      {renderLinks().map((link, index) => (
+        <LinkItem 
+          key={link.id} 
+          showCount={true} 
+          link={link} 
+          index={index + 1} 
+        />
+      ))}
     </div>
   );
 }
